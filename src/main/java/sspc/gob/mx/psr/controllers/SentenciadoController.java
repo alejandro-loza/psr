305db3aca8da5be293e48740b582235e87416fce
Pinky <alejandro.hernandez@finerio.mx>
2021-12-21 17:58:11 -0600

    Agregue cors y quite unique a nombre

--- src/main/java/sspc/gob/mx/psr/controllers/SentenciadoController.java
+++ src/main/java/sspc/gob/mx/psr/controllers/SentenciadoController.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sspc.gob.mx.psr.services.SentenciadoService;
 import sspc.gob.mx.psr.validator.SentenciadoValidador;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/sentenciado")
 @RestController
 public class SentenciadoController {
